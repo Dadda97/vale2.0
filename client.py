eb87f9ffdaadfb2d9977251bf0748b911ef7b5c5
Daniele <dan>
2020-12-16 23:30:08 +0100

    WIP on server

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import socket
 import sys
 
 if len(sys.argv) > 1:
-    HOST = 'wss://gentle-dawn-03506.herokuapp.com/0.0.0.0'  # The server's hostname or IP address
+    HOST = 'gentle-dawn-03506.herokuapp.com'  # The server's hostname or IP address
     PORT = int(sys.argv[1])            # The port used by the server
 else:
     HOST = '127.0.0.1'  # The server's hostname or IP address
--- server.py
+++ server.py
@@ -11,13 +11,12 @@ host =  "127.0.0.1"  if len(sys.argv) > 1 else "0.0.0.0"
 server.bind((host, port))
 print(f"###### SERVER RUNNING ON PORT {port} ({host}) ######")
 server.listen()
-# should wait for an actual connection, but fires right away
+
 while True:
 
     s, addr = server.accept()
     print("Recived request from:", addr)
     
-    # should wait to receive "Hello, world! (from client)", but receives b''
     print(addr, " sent: ", repr(s.recv(1024)))
     
     print("Answering to:", addr)
@@ -26,4 +25,4 @@ while True:
     
     print("Answered to:", addr)
 
-s.close()
+    s.close()
